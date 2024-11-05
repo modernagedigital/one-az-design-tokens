5da10dbb60ef4304b9ff7838c6a2bc50daf54f89
Alan Brown <alan@alanbrown.design>
2024-11-05 16:40:00 +0000

    updated scss

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ export default function App() {
     <div className="App">
       <div className="t_font-size--9xl">Hello CodeSandbox</div>
       <h2>Start editing to see some magic happen!</h2>
-      <div style={{ background: "green" }} className="size-fluid"></div>
+      <div className="size-fluid-lg"></div>
+      <div className="size-fluid-xl"></div>
+      <div className="size-fluid-2xl"></div>
+      <div className="size-fluid-3xl"></div>
     </div>
   );
 }
