3b3c19b6d689672863e6c80344fd44bb3819eb4a
Alan Brown <102808405+alanbrowndesign@users.noreply.github.com>
2024-11-05 12:20:52 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles/styles.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="t_font-size--9xl">Hello CodeSandbox</div>
+      <h2>Start editing to see some magic happen!</h2>
+      <div style={{ background: "green" }} className="size-fluid"></div>
+    </div>
+  );
+}
